7cf00225295dd72f614a1181f05fd1311eea2054
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-01 00:48:31 +0530

    Protected route part done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,8 +3,12 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import NavBar from "./components/navbar/Navbar";
 import LeftBar from "./components/leftBar/LeftBar";
-import { Outlet } from "react-router-dom"; // Ensure correct import
+import { Outlet, Navigate } from "react-router-dom"; // Ensure correct import
 import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+const currentUser = true;
 
 const Layout = () => {
   return (
@@ -19,12 +23,30 @@ const Layout = () => {
   );
 };
 
+const ProtectedRoute = ({ children }) => {
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+  return children;
+};
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         {/* Wrap routes in the Layout */}
-        <Route path="/" element={<Layout />}></Route>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Layout />
+            </ProtectedRoute>
+          }
+        >
+          <Route index element={<Home />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Route>
+        {/* Public Routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
